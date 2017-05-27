35c3e0065c36dbe30451778550d6eaf8ba34cb98
carlosdomenje <carlosdomenje@hotmail.com>
2017-05-27 09:25:03 -0300

    Correccion de nombre de funciones

--- Ex_1/src/func_maths.h
+++ Ex_1/src/func_maths.h
@@ -78,12 +78,12 @@ extern int32_t funcion(uint8_t a, uint32_t b);
  **/
 extern uint16_t suma (uint16_t a, uint16_t b);
 
-/** \brief Funcion Suma
+/** \brief Funcion Resta
  **
  **
  **
- ** \param[in]    a primer sumando
- ** \param[in]    b segundo sumando
+ ** \param[in]    a primer Restando
+ ** \param[in]    b segundo Restando
  ** \return E_OK  returns ...
  **/
 extern uint16_t resta (uint16_t a, uint16_t b);
